2fd04bf0c54ecb627fdbf827ee36e0900d14fe4d
José Eugênio dos Santos <37482325+J-Eugenio@users.noreply.github.com>
2020-04-04 13:52:18 -0300

    Add files via upload

---
+++ knexfile.js
@@ -0,0 +1,16 @@
+module.exports = {
+  client: 'postgresql',
+  connection: {
+    database: 'tasks',
+    user: 'postgres',
+    password: 'kqklpi123'
+  },
+  pool: {
+    min: 2,
+    max: 10
+  },
+  migrations: {
+    tableName: 'knex_migrations'
+  }
+
+};
